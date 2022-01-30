2c4a57f8e14bfbcdb1b838ec8ff0623bf61f1544
quyen-tran <quyen.devtech@gmail.com>
2022-01-30 14:09:00 -0800

    Added dynamic features to Portfolio index.html template

--- django_prj/myproject/myapp/views.py
+++ django_prj/myproject/myapp/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
-def index(request): # assigned to homepage
-    return render(request, 'index.html')
+def index(request):
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Feature Name 1'
+    feature1.details = '1-Ipsum something sommathing...'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Feature Name 2'
+    feature2.details = '2-Ipsum something sommathing...'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Feature Name 3'
+    feature3.details = '3-Ipsum something sommathing...'
+
+    features = [feature1, feature2, feature3]
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     text = request.POST['text']
