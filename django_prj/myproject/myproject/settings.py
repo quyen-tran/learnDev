69bf05b3ab90256261434f0a7c65efd4d763bf50
quyen-tran <quyen.devtech@gmail.com>
2022-01-30 11:19:46 -0800

    Redoing commits

--- django_prj/myproject/myproject/settings.py
+++ django_prj/myproject/myproject/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9m9ku6achd+_#b(k8+$1p7vfhsi*cuw(#s5$hx@a9kvbjoke!d'
+# REMOVED SECRET KEY THAT WAS HERE
+from decouple import config
+SECRET_KEY = config("SECRET KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
