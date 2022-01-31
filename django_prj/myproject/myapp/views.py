9e1e941688a05ae3081b1744172e6c3274f30e37
quyen-tran <quyen.devtech@gmail.com>
2022-01-30 17:28:34 -0800

    Added new functionality to the site
    Added registration, login, logout, and authentication

--- django_prj/myproject/myapp/views.py
+++ django_prj/myproject/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -7,7 +9,45 @@ def index(request):
     features = Feature.objects.all
     return render(request, 'index.html', {'features': features})
 
-def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount' : amount_of_words})
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirm']
+        if password_confirm == password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already in use')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already in use')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect('login')
+    else:    
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
